1b959480e9be5e087e423c496709be927cfc3405
Jacob Barber <jacob.barber@disneystreaming.com>
2022-05-04 13:26:51 -0500

    undo main changes

--- src/main.ts
+++ src/main.ts
@@ -302,7 +302,7 @@ Object.values(WeaponType).map(r => {
       ALL_WEAPONS
         .filter(x => Array.from(selectedWeaponTypes).every((y: WeaponType) => x.weaponTypes.includes(y)))
         .forEach(w => selectedWeapons.add(w))
-      
+
     }
     updateWeaponCheckboxes();
     redraw();
